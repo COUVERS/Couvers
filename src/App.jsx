2137db80ef8f39a7581a766c90ccb653e4ee475f
Riri022 <rgoto01@mylangara.ca>
2026-02-11 16:18:38 -0800

    Initial navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navigation from './Navigation'
 
 function App() {
 
   return (
     <>
       <h1>This is a Render</h1>
+      <Navigation />
     </>
   )
 }
---
+++ src/Navigation.jsx
@@ -0,0 +1,13 @@
+export default function Navigation() {
+    return (
+        <>
+            <h1>This is Navigation</h1>
+            <ul>
+                <li>Home</li>
+                <li>Course</li>
+                <li>Account</li>
+                <li>Sign Out</li>
+            </ul>
+        </>
+    )
+}
