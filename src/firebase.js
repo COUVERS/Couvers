c8cbf35be764915036479bb237bc02175187bae2
Riri022 <rgoto01@mylangara.ca>
2026-02-19 01:09:51 -0800

    implement test certificate component and download pdf

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
+import { getStorage } from "firebase/storage";
+
 // Firebase configuration
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,3 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
