6697b084592b973dee87cae2003441e34b51296a
Riri022 <rgoto01@mylangara.ca>
2026-02-22 20:00:22 -0800

    import MUI icons and components

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,22 @@
 import './Nav.css'
+import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import {
+    Box,
+    MuiDrawer,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    IconButton
+} from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import SchoolIcon from '@mui/icons-material/School';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 export default function Navigation() {
     return (
