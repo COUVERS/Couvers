b50e057be2eb2e6d432e0d85d74e197c9afc6621
Bella-moonju <mra01@mylangara.ca>
2026-03-06 14:11:08 -0800

    Resolve type mistake

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import "./auth.css"
+import "../auth.css"
 
 export default function LoginForm({ onGoSignup, onLoginSuccess }) {
     const [email, setEmail] = useState("")
