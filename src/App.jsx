06b57b16928df9942f05b42cd31b2f3d57960672
carlosmtz <carlossaul2996@gmail.com>
2026-03-06 20:37:50 -0800

    quiz style

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
+import { useState } from "react"
 import CustomButton from './Reusable-Components/CustomButton'
 import Navigation from './components/Navigation'
 import CertificateTest from './pages/CertificateTest'
 import Quiz from './components/Quiz'
 import { quizQuestions } from './services/quizData'
 
-function App() {
+export default function App() {
+  const [page, setPage] = useState("home")
+  const currentQuestion = quizQuestions[0]
 
-   const currentQuestion = quizQuestions[0]
+  return (
+    <>
+      {/* <div style={{ display: "grid", placeItems: "center", minHeight: "100vh" }}>
+        {mode === "login" ? (
+          <LoginForm onGoSignup={() => setMode("signup")} />
+        ) : (
+          <SignupForm onGoLogin={() => setMode("login")} />
+        )}
+      </div> */}
 
- return (
-    <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", height: "100vh" }}>
 
-      <Navigation />
+        <Navigation
+          page={page}
+          setPage={setPage}
+          forceCollapsed={page === "courses"}
+        />
 
-      <div style={{ flex: 1, padding: "40px" }}>
+        {/* {page === "courses" && <CourseNavigation />} */}
 
-        <h1>This is a set up</h1>
+        <main style={{ flex: 1, padding: page === "courses" ? 0 : 16 }}>
+          {page === "home" && <h1>Home</h1>}
+          {page === "courses" && <Course />}
+          
+          <div style={{ padding: "40px" }}>
+            <h1>This is a set up</h1>
+            <CustomButton />
+          </div>
 
-        <CustomButton />
+        </main>
 
         <Quiz
           question={currentQuestion}
@@ -27,10 +48,15 @@ function App() {
 
         <CertificateTest />
 
-      </div>
+        {/* <LecturePage }
+          lessons={lessons}
+          activeLessonId={activeLessonId}
+          onSelectLesson={setActiveLessonId}
+          onExit={() => console.log("Exit lecture")}
+          onTakeQuiz={(lessonId) => console.log("Take quiz for:", lessonId)}
+        {/>  */}
 
-    </div>
+      </div>
+    </>
   )
 }
-
-export default App
