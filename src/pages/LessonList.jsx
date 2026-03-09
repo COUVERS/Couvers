69941b1845ad2f0cf981d01da090c67bcea01477
Riri022 <rgoto01@mylangara.ca>
2026-03-09 11:31:52 -0700

    First draft of lessonLinkBtn component

--- src/pages/LessonList.jsx
+++ src/pages/LessonList.jsx
@@ -1,6 +1,7 @@
 import Box from "@mui/material/Box"
 import LessonListItem from "../components/features/LessonListItem"
 
+
 export default function LessonList({ lessons = [], onOpenLesson }) {
     return (
         <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
