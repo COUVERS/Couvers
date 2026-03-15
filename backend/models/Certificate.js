dbe40613f0dcf384165706c089784eb0dfd39499
Riri022 <rgoto01@mylangara.ca>
2026-03-15 01:35:36 -0700

    create certificate modal and API path

---
+++ backend/models/Certificate.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const certificateSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        courseId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Course",
+            required: true,
+        },
+        issuedAt: {
+            type: Date,
+            default: Date.now,
+        },
+        fileUrl: {
+            type: String,
+            default: "",
+        },
+    },
+    { timestamps: true }
+);
+
+certificateSchema.index({ userId: 1, courseId: 1 }, { unique: true });
+
+module.exports = mongoose.model("Certificate", certificateSchema);
