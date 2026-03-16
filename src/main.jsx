cf47394c8e7ff61124f8bf4a633c4961070b0c02
Bella-moonju <mra01@mylangara.ca>
2026-03-16 11:53:42 -0700

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './global.css'
-import { ThemeProvider } from '@mui/material/styles'
-import theme from './styles/theme.js'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App.jsx"
+import "./global.css"
+import { ThemeProvider } from "@mui/material/styles"
+import theme from "./styles/theme.js"
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>
 )
