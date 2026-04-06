4df68a54a488746fc008b858035ae8d91e8f3854
Riri022 <rgoto01@mylangara.ca>
2026-04-05 19:05:11 -0700

    Add: Review btn drop shadow

--- src/components/reusable-ui/ReviewCourseLinkButton.jsx
+++ src/components/reusable-ui/ReviewCourseLinkButton.jsx
@@ -26,6 +26,8 @@ const ReviewButton = styled(Button, {
     textTransform: "none",
     display: "flex",
     alignItems: "stretch",
+    boxShadow:
+        "0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.20)",
     "@media (min-width:899px) and (max-width:1305px)": {
         minHeight: "96px",
     },
