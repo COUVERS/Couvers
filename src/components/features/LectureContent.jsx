a0ec73c62e8b47936a22490f0a0e8436da9a92c4
Riri022 <rgoto01@mylangara.ca>
2026-04-05 17:35:06 -0700

    Modified section header font size on Lecture page

--- src/components/features/LectureContent.jsx
+++ src/components/features/LectureContent.jsx
@@ -37,11 +37,13 @@ const StyledSummary = styled(AccordionSummary)(() => ({
     },
 }))
 
-const SectionHeading = styled(Box)(() => ({
-    fontSize: "28px",
+const SectionHeading = styled(Box)(({ theme }) => ({
+    fontSize: "24px",
     fontWeight: 600,
     color: "var(--Color-Text-Primary)",
-    marginTop: "16px",
+    [theme.breakpoints.up("md")]: {
+        fontSize: "28px",
+    },
 }))
 
 const Paragraph = styled(Box)(() => ({
