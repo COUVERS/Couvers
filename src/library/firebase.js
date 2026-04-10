380924e95c247e820d852dcc825ff046e7a8640d
Bella-moonju <mra01@mylangara.ca>
2026-04-10 15:04:33 -0700

    Update firebase.js

--- src/library/firebase.js
+++ src/library/firebase.js
@@ -11,6 +11,8 @@ const firebaseConfig = {
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
+console.log("FIREBASE STORAGE BUCKET:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET)
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 
